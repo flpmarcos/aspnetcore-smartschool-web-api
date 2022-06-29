cd1f9fb5eb764007fd76a00eaf1aa30da7f475b8
Felipe Eufranio <felipe.eufranio@bs2tecnologia.com.br>
2022-06-29 19:11:49 -0300

    Criando DataContext

--- SmartSchool.WebAPI/Data/SmartContext.cs
+++ SmartSchool.WebAPI/Data/SmartContext.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Model;
+
 namespace SmartSchool.WebAPI.Data
 {
-    public class SmartContext
+    public class SmartContext : DbContext
     {
-        
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public DbSet<Professor> Professores { get; set; }
+
+        public DbSet<Disciplina> Disciplinas { get; set; }
+
+        public DbSet<AlunoDisciplina> AlunoDisciplinas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new { AD.AlunoId, AD.DisciplinaId });
+        }
+
     }
 }
