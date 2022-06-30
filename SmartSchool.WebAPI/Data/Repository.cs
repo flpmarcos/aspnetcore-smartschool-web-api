de1c471bae1792167f47daefa254252619a8d507
Felipe Eufranio <felipe.eufranio@bs2tecnologia.com.br>
2022-06-30 16:11:29 -0300

    39. Métodos Add, Update e Delete

--- SmartSchool.WebAPI/Data/Repository.cs
+++ SmartSchool.WebAPI/Data/Repository.cs
@@ -2,6 +2,31 @@
 {
     public class Repository : IRepository
     {
+        private readonly SmartContext _context;
+        public Repository(SmartContext context)
+        {
+
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() > 0);
+        }
 
     }
 }
