a98540b6d7bc44470abf51a7118d32482ed4b368
Felipe Eufranio <felipe.eufranio@bs2tecnologia.com.br>
2022-07-01 11:18:00 -0300

    54. Auto Mapper e Profile;

---
+++ SmartSchool.WebAPI/Helpers/SmartSchoolProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SmartSchool.WebAPI.Dtos;
+using SmartSchool.WebAPI.Model;
+
+namespace SmartSchool.WebAPI.Helpers
+{
+    public class SmartSchoolProfile : Profile
+    {
+        public SmartSchoolProfile()
+        {
+            CreateMap<Aluno, AlunoDto>()
+                .ForMember(
+                    dest => dest.Nome,
+                    opt => opt.MapFrom(src => $"{src.Nome} {src.Sobrenome}")
+                );
+        }
+    }
+}
