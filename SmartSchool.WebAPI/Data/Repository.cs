06e310d65fda8dfcd68e55fef5a2b926f679aaae
Felipe Marcos <flpmarcos>
2022-06-30 22:11:43 -0300

    43. Métodos GetAluno - Parte 02

--- SmartSchool.WebAPI/Data/Repository.cs
+++ SmartSchool.WebAPI/Data/Repository.cs
@@ -1,4 +1,6 @@
-﻿using SmartSchool.WebAPI.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Model;
+using System.Linq;
 
 namespace SmartSchool.WebAPI.Data
 {
@@ -32,19 +34,65 @@ namespace SmartSchool.WebAPI.Data
         }
 
         // Alunos
-        public Aluno[] GetAllAlunos()
+        public Aluno[] GetAllAlunos(bool includeProfessor = false)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeProfessor)
+            {
+                // Aqui estou fazendo leftjoin inicialmente com a disciplina do aluno e depois com o professor.
+                query = query.Include(a => a.AlunosDisciplinas)
+                     .ThenInclude(ad => ad.Disciplina)
+                     .ThenInclude(d => d.Professor);
+            }
+            query = query.AsNoTracking().OrderBy(a => a.Id);
+
+            return query.ToArray();
+
         }
 
-        public Aluno[] GetAllAlunosByDisciplinaId()
+        public Aluno[] GetAllAlunosByDisciplinaId(int disciplinaId, bool includeProfessor = false)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeProfessor) 
+            {   
+                // left join com disciplina e professor
+                query = query.Include(a => a.AlunosDisciplinas)
+                             .ThenInclude(ad => ad.Disciplina)
+                             .ThenInclude(d => d.Professor);
+            }
+
+            // asnotrancking para não ficar bloqueado
+            // order by e where baseado no id da disciplina
+            query = query.AsNoTracking()
+                         .OrderBy(a => a.Id)
+                         .Where(aluno => aluno.AlunosDisciplinas.Any(ad => ad.DisciplinaId == disciplinaId));
+
+            return query.ToArray();
         }
 
-        public Aluno[] GetAlunoById()
+        // É Aluno e nao Aluno[] porque ele vai retornar um 
+        public Aluno GetAlunoById(int alunoId, bool includeProfessor = false)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeProfessor) 
+            {
+                // Leftjoin com disciplina e professor baseado no parametro
+                query = query.Include(a => a.AlunosDisciplinas)
+                           .ThenInclude(ad => ad.Disciplina)
+                           .ThenInclude(d => d.Professor);
+            }
+
+            // where baseado no id aluno
+            query = query.AsNoTracking()
+                         .OrderBy(a => a.Id)
+                         .Where(aluno => aluno.Id == alunoId);
+
+            // FirstorDefault -> funcao do entity para pegar o primeiro
+            return query.FirstOrDefault();
+           
         }
 
         // Professores
