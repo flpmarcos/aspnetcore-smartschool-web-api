eea8883ded7911011f307ae3de72bb7833811298
Felipe Eufranio <felipe.eufranio@bs2tecnologia.com.br>
2022-07-01 15:26:02 -0300

    60. Enviando ao GIT; Encerramento de Seção 6:DTO e AutoMapper;

--- SmartSchool.WebAPI/Controllers/ProfessorController.cs
+++ SmartSchool.WebAPI/Controllers/ProfessorController.cs
@@ -1,7 +1,10 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SmartSchool.WebAPI.Data;
+using SmartSchool.WebAPI.Dtos;
 using SmartSchool.WebAPI.Model;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SmartSchool.WebAPI.Controllers
@@ -12,68 +15,85 @@ namespace SmartSchool.WebAPI.Controllers
     {
         
         private readonly IRepository _repo;
+        private readonly IMapper _mapper;
 
-        public ProfessorController(IRepository repo)
+        public ProfessorController(IRepository repo, IMapper mapper)
         {
            _repo = repo;
+            _mapper = mapper;
         }
 
 
         [HttpGet]
         public IActionResult Get()
         {
-            var result = _repo.GetAllProfessores(true);
-            return Ok(result);
+            var professores = _repo.GetAllProfessores(true);
+            return Ok(_mapper.Map<IEnumerable<ProfessorDto>>(professores));
         }
 
-        
+        [HttpGet("getRegister")]
+        public IActionResult GetRegister()
+        {
+
+            return Ok(new ProfessorRegistrarDto());
+        }
+
+
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
             var professor = _repo.GetProfessorById(id,true);
             if (professor == null) return BadRequest("Professor não foi encontrado!");
 
-            return Ok(professor);
+            var professorDto = _mapper.Map<AlunoDto>(professor);
+
+            return Ok(professorDto);
+
         }
 
 
         [HttpPost]
-        public IActionResult Post(Professor professor)
+        public IActionResult Post(ProfessorRegistrarDto model)
         {
+            var professor = _mapper.Map<Professor>(model);
+
             _repo.Add(professor);
             if (_repo.SaveChanges())
             {
-                return Ok(professor);
+                return Created($"/api/aluno/{model.Id}", _mapper.Map<ProfessorDto>(professor));
             }
             return BadRequest("Houve uma falha ao salvar o Professor!");
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id, Professor professor)
+        public IActionResult Put(int id, ProfessorRegistrarDto model)
         {
             var prof = _repo.GetProfessorById(id);
             if (prof == null) return BadRequest("Professor nao encontrado!");
 
+            _mapper.Map(model, prof);
+
             _repo.Update(prof);
             if (_repo.SaveChanges())
             {
-                return Ok(prof);
+                return Created($"/api/aluno/{model.Id}", _mapper.Map<ProfessorDto>(prof));
             }
             return BadRequest("Houve uma falha ao atualizar o Professor!");
             
         }
 
         [HttpPatch("{id}")]
-        public IActionResult Path(int id, Professor professor)
+        public IActionResult Path(int id, ProfessorRegistrarDto model)
         {
             var prof = _repo.GetProfessorById(id);
             if (prof == null) return BadRequest("Professor nao encontrado!");
 
+            _mapper.Map(model, prof);
 
             _repo.Update(prof);
             if (_repo.SaveChanges())
             {
-                return Ok(prof);
+                return Created($"/api/aluno/{model.Id}", _mapper.Map<ProfessorDto>(prof));
             }
             return BadRequest("Houve uma falha ao atualizar o Professor!");
 
---
+++ SmartSchool.WebAPI/Dtos/ProfessorDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmartSchool.WebAPI.Dtos
+{
+    public class ProfessorDto
+    {
+
+        public int Id { get; set; }
+
+        public int Registro { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataIni { get; set; } 
+
+        public bool Ativo { get; set; } = true;
+
+    }
+}
