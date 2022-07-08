44c2c05748a2a511ad402f699be52c490ca910f5
Felipe Eufranio <felipe.eufranio@bs2tecnologia.com.br>
2022-07-08 17:32:42 -0300

    96. Explicando Arquivos;97. Estruturando projeto;98. Referência de Components;99. NAV e Título;100. Configurando Rotas;101. Alunos e Referência Module;102. Services e Models

--- SmartSchoolApp/src/app/app.module.ts
+++ SmartSchoolApp/src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    DashboardComponent,
+    PerfilComponent,
+    ProfessoresComponent,
+    NavComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    NgxSpinnerModule,
+    ToastrModule.forRoot({
+      timeOut: 3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar:true,
+      closeButton: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- SmartSchoolApp/src/environments/environment.ts
+++ SmartSchoolApp/src/environments/environment.ts
@@ -2,9 +2,12 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  mainUrlAPI: 'http://localhost:5002/api/'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
